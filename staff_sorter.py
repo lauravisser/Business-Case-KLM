327e89787c255b9ef7fae92857f4eacfb9b278bd
lauravisser <42065636+lauravisser@users.noreply.github.com>
2021-04-15 12:21:38 +0200

    Update staff_sorter.py

--- staff_sorter.py
+++ staff_sorter.py
@@ -10,6 +10,8 @@ data = pd.read_csv("Staff.csv", sep = ";")
 data.boxplot(column = "Actual BD teams", by = "Shift")
 
 cleaned_data = data.drop(columns = ["DayOfMonth", "Year"])
-grouped_mean = cleaned_data.groupby(["Shift", "Month"]).mean()
-cleaned_data.plot(x = "Month", y = "Actual BD teams", kind = "bar")
+grouped_mean = cleaned_data.groupby(["Shift", "DayOfWeek"]).mean()
 grouped_mean
+
+teamsAvailable = np.empty([7,3], dtype = float)
+teamsAvailable
